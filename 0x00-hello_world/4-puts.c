067bda69139e2e78d69324b7431a42507c9f7a5c
Patience-Sibanda <psa.khanye@gmail.com>
2023-10-05 14:25:15 -0700

    Files

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry of the program
+ *
+ * Return:  0(Success)
+ */
+int main()
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
